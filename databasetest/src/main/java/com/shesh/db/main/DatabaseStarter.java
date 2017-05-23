b978b9ba31880ce059dcd5ca9528e53beed97821
vshes <vshes.60@gmail.com>
2017-05-23 18:18:48 -0500

    Property file usage for JDBC and Hibernate Config

--- databasetest/src/main/java/com/shesh/db/main/DatabaseStarter.java
+++ databasetest/src/main/java/com/shesh/db/main/DatabaseStarter.java
@@ -2,17 +2,23 @@ package com.shesh.db.main;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.*;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 /**
  * Created by shesh on 5/23/17.
  */
 public class DatabaseStarter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Logger logger = Logger.getLogger("MainLogger");
         logger.info("Process Starting.. ");
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("dbdemo");
+        Properties prop = new Properties();
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream("config.properties");
 
+        prop.load(inputStream);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("dbdemo",prop);
+        logger.info("test complete");
 
     }
 }
